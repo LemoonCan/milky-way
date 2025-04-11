ebc5a5b110fbe4c2c88f7d96b515ee15336bfe5d
Lemoon Can <lemoon.can@gmail.com>
2025-04-11 23:32:55 +0800

    feat: 🎸 初始化

---
+++ milky-way-core/src/test/java/lemoon/can/milkyway/MilkyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.lemoon.can.milkyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkyWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
