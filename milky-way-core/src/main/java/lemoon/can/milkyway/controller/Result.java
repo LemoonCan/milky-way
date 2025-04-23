584a9cf0f2bfeed136fdbda6ba1aef9bc687a53e
Lemoon Can <lemoon.can@gmail.com>
2025-04-23 21:24:48 +0800

    feat: 🎸 定义通用result

---
+++ milky-way-core/src/main/java/lemoon/can/milkyway/controller/Result.java
@@ -0,0 +1,33 @@
+package lemoon.can.milkyway.controller;
+
+import lombok.Data;
+
+/**
+ * @author lemoon
+ * @since 2025/4/23
+ */
+@Data
+public class Result<T> {
+    private String code;
+    private String msg;
+    private T data;
+
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>();
+        result.setCode("success");
+        result.setMsg("成功");
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> fail(String code, String msg) {
+        Result<T> result = new Result<>();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+    public static <T> Result<T> fail(String msg) {
+        return fail("fail", msg);
+    }
+}
