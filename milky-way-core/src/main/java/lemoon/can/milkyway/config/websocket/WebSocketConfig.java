20e43478874b2afdb8822fa341c3d70415f5ab54
Lemoon Can <lemoon.can@gmail.com>
2025-05-21 22:10:22 +0800

    docs: ✏️ STOMP协议通常约定的路径

--- milky-way-core/src/main/java/lemoon/can/milkyway/config/websocket/WebSocketConfig.java
+++ milky-way-core/src/main/java/lemoon/can/milkyway/config/websocket/WebSocketConfig.java
@@ -1,8 +1,6 @@
 package lemoon.can.milkyway.config.websocket;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,6 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 /**
  * WebSocket配置类
  * 配置WebSocket和STOMP协议相关的设置
+ *
  * @author lemoon
  * @since 2025/5/20
  */
@@ -18,15 +17,33 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * 声明此配置后，无需显示声明具体的订阅路径，就可接收相应前缀的订阅路径
+     * 广播地址前缀，通常约定topic用作广播前缀，queue用作点对点前缀
+     * 广播：
+     * 1.订阅路径：/topic/{主题名}/{ID}
+     * 2.推送路径：/topic/{主题名}/{ID}
+     * 点对点：
+     * 1.订阅路径：/user/queue/{主题名}
+     * 2.推送路径：/user/{userID}/queue/{主题名}
+
+     * 客户端发送消息前缀，通常约定为/app
+     *
+     * @param config MessageBrokerRegistry
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // 广播地址前缀
-        config.setApplicationDestinationPrefixes("/app"); // 客户端发送消息前缀
+        //广播地址前缀
+        config.enableSimpleBroker("/topic", "/queue");
+        //客户端发送消息前缀
+        config.setApplicationDestinationPrefixes("/app");
+        //用户专属消息路径
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") // WebSocket 连接端点
+        registry.addEndpoint("/ws") // WebSocket 连接端点，仅在建立连接时使用此前缀
                 .setAllowedOrigins("*") // 允许跨域
                 .withSockJS(); // 启用 SockJS fallback，兼容不支持WebSocket的浏览器
     }
